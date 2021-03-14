0efce4806ba71b01a719768b9fa40525c717d688
Lakshmipriya632 <80029742+Lakshmipriya632@users.noreply.github.com>
2021-03-14 18:38:14 +0530

    Update mycode.java

--- mycode.java
+++ mycode.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 class mycode {
      public static void main(String[] args) {
          System.out.println("Hello this is my first jenkins job");
-         System.out.println("First commit is done");
+         System.out.println("First commit is made");
      }
 }
